552ae5f3afa4b40c9c7e60af1e88b880066ec3c1
GlobyteFranco <franco.cipulli@globallogic.com>
2026-04-21 12:00:55 -0300

    Casi finalizacion de clase Fraccion y comienzo de interfaz calculadora y clase calculadoraFraccion

--- App.java
+++ App.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println(
+        // "Binvenido/a la calculadora basica de fraccions!! \n\n Por favor ingrese dos
+        // operandos, uno despues del otro a traves del enter para empezar a probar");
+        // int operandA = sc.nextInt();
+        // int operandB = sc.nextInt();
+        // Fraccion miFraccion = new Fraccion(operandA, operandB);
+        // System.out.println(
+        // miFraccion + "\n\n\n A continuacion aparcera la version simplificada" +
+        // miFraccion.simplicarFraccion());
+        int variable = -3;
+        System.out.println("Probando igualacion de fracciones" + new Fraccion(82, 9).equals(new Fraccion(9, 1)));
     }
 }
---
+++ Fraccion.java
@@ -0,0 +1,116 @@
+public class Fraccion {
+    private final int numerador;
+    private final int denominador;
+
+    public Fraccion(int numerador, int denominador) {
+
+        validarDenominadorNulo(denominador);
+        if (denominador < 0) {
+            denominador = denominador * -1;
+            numerador = numerador * -1;
+        }
+        this.numerador = numerador;
+        this.denominador = denominador;
+    }
+
+    public Fraccion(float numerador, float denominador) { // Necesita que decimalToFraction este habilitado ademas de
+                                                          // agregarla mas logica
+        validarDenominadorNulo(denominador);
+
+        this.numerador = (int) numerador;
+        this.denominador = (int) denominador;
+    }
+
+    // public static Fraccion decimalToFraction(float operando) { //Falta la
+    // implementacion de la calculadora
+
+    // if (operando == 0) {
+    // return new Fraccion(0, 1);
+    // }
+
+    // Fraccion fractionReturned;
+    // float parteDecimal; // los va a volver una fraccion entre si
+    // int parteEntera;
+
+    // parteEntera = (int) operando;
+    // parteDecimal = operando - parteEntera;
+
+    // if (parteDecimal != 0) {
+    // float aux = 100 / parteDecimal;
+    // Fraccion fraccionParteDecimal = new Fraccion(100, aux);// Nueva fraccion con
+    // la parte decimal
+    // if (parteEntera != 0) {
+    // Fraccion fraccionParteEntera = new Fraccion(Math.abs(parteEntera), 1);
+    // // return fractionReturned = fraccionParteDecimal + fraccionParteEntera *
+    // // (Math.abs(parteEntera) / parteEntera); //Falta la implementacion
+
+    // }
+    // fractionReturned = fraccionParteDecimal;
+    // return fractionReturned;
+    // } else if (parteEntera != 0) {
+    // Fraccion fraccionParteEntera = new Fraccion(parteEntera, 1);
+
+    // fractionReturned = fraccionParteEntera;
+    // return fractionReturned;
+    // }
+
+    // }
+
+    private void validarDenominadorNulo(float denominador) {
+        if (denominador == 0) {
+            throw new IllegalArgumentException("El denominador NO puedo ser cero (0)");
+        }
+    }
+
+    public Fraccion simplicarFraccion() {
+        int valorMCD = mcd(this.numerador, this.denominador);
+
+        return new Fraccion(this.numerador / valorMCD, this.denominador / valorMCD);
+    }
+
+    public int mcd(int a, int b) {
+        a = Math.abs(a);
+        b = Math.abs(b);
+
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
+    }
+
+    public int mcm(int operandoA, int operandoB) {
+
+        return (Math.abs(operandoB * operandoA)) / (mcd(operandoA, operandoB));
+
+    }
+
+    public int getNumerador() {
+        return numerador;
+    }
+
+    public int getDenominador() {
+        return denominador;
+    }
+
+    @Override
+    public String toString() {
+        return "\n" + String.valueOf(this.numerador) + "\n-\n" + String.valueOf(this.denominador) + "\n";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        boolean resultado = false;
+        if (obj instanceof Fraccion) {
+            Fraccion miFraccionComparar = (Fraccion) obj;
+            if (this.simplicarFraccion().numerador == miFraccionComparar.simplicarFraccion().numerador
+                    && this.simplicarFraccion().denominador == miFraccionComparar.simplicarFraccion().denominador) {
+                resultado = true;
+            }
+        }
+        return resultado;
+    }
+
+}
